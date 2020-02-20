4417d43e2a5d900e45a096378f908479fca524a7
Etienne Soulard-Geoffrion <etienne.soulard-geoffrion@clinia.ca>
2020-02-20 14:25:05 -0500

    chore: remove angular workspace

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,12 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../stories/**/*.stories.ts'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-notes'],
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.alias['@clinia/angular-vision'] = path.resolve(__dirname, '../src');
+
+    // Return the altered config
+    return config;
+  },
 };
